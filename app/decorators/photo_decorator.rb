52f63a188e41e4fc674ab9f52b758f4db848f7bb
Veneta Dencheva <venetadencheva@gmail.com>
2012-05-19 19:45:47 +0300

    Fix next and previous photo bug

--- app/decorators/photo_decorator.rb
+++ app/decorators/photo_decorator.rb
@@ -2,10 +2,10 @@ class PhotoDecorator < ApplicationDecorator
   decorates :photo
 
   def previous_photo_tag
-    photo.previous_photo.try { |p| h.link_to I18n.t('photos.show.previous'), h.album_photo_path(album, p), class: "prevPhoto" }
+    photo.previous_photo.try { |p| h.link_to I18n.t('photos.show.previous'), h.album_photo_path(album, p), class: "leftPhotoLink" }
   end
 
   def next_photo_tag
-    photo.next_photo.try { |p| h.link_to I18n.t('photos.show.next'), h.album_photo_path(album, p), class: "nextPhoto" }
+    photo.next_photo.try { |p| h.link_to I18n.t('photos.show.next'), h.album_photo_path(album, p), class: "rightPhotoLink" }
   end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :user
-  has_many :photos, dependent: :destroy
+  has_many :photos, dependent: :destroy, order: 'photos.id DESC'
   has_many :comments, through: :photos
 
   validates_presence_of :title
